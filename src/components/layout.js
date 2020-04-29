4ec772c614353dfc5f3a7be9a69bd320c6a74949
Ethan Zoller <ezoller11@gmail.com>
2020-04-29 03:20:34 -0700

    Removed gatsby junk

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,21 +1,10 @@
 // Library and Package Imports
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
 
 import "./layout.css"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
   return (
     <>
       <main>{children}</main>
