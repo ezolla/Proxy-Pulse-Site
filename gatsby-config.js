8c7f3a1d39f0d6bae144e74650f9d39362017c4f
Ethan Zoller <ezoller11@gmail.com>
2020-04-28 22:55:19 -0700

    Edited meta data and added favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Proxy Pulse`,
+    description: `Proxy Pulse is open-sourced software that allows for simple and easy testing of all types of proxies on any website you desire.`,
+    author: `Ethan Zoller`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -22,10 +22,10 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#6383FF`,
+        theme_color: `#6383FF`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.svg`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
