75a951211969ad6c57284a3d2a2feeedf7674111
Ethan Zoller <ezoller11@gmail.com>
2020-04-28 23:01:40 -0700

    Removed gatsby boilerplate junk

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,10 +1,4 @@
-/**
- * SEO component that queries for data with
- *  Gatsby's useStaticQuery React hook
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
+// Library and Package Imports
 import React from "react"
 import PropTypes from "prop-types"
 import { Helmet } from "react-helmet"
